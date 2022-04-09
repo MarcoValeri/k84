aeb810d95cc76af1d919beaca2fff84049adc0c0
Marco Valeri <marcovaleri@hotmail.it>
2022-04-09 12:46:49 +0100

    Add layout templates

--- views/home.php
+++ views/home.php
@@ -1,1 +1,12 @@
+<?php 
+// Set title of the page
+$title = 'Home';
+
+// Include header.php
+include_once pathView('layout/head.php');
+?>
 <h2>Kitchen 1984</h2>
+<?php
+// Include footer.php
+include_once pathView('layout/footer.php');
+?>
